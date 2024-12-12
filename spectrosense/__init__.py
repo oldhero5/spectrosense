b1bff8b42b047dcc23a40e89d60deb338a454b52
oldhero5 <oldhero5@proton.me>
2024-12-12 16:39:26 -0500

    bump version to 0.2.1

--- spectrosense/__init__.py
+++ spectrosense/__init__.py
@@ -2,3 +2,4 @@ from .processor import SignalProcessor
 from .analyzer import SignalAnalyzer
 from .visualizer import SpectrogramVisualizer
 from .ai_integration import AIAnalyzer
+__version__ = "0.2.1"
